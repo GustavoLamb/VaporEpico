b0041cf5dd8a0752ee2248ded768affa2456c746
Gustavo Lamb <gustavo.lamb@uol.com.br>
2021-02-27 11:57:06 -0300

    Adicionando tela de cadastro

---
+++ src/main/java/br/com/ifsul/vaporepico/domain/Fontes.java
@@ -0,0 +1,12 @@
+package br.com.ifsul.vaporepico.domain;
+
+import java.awt.*;
+
+/* Interface para definição das fontes usadas no projeto */
+
+public interface Fontes {
+
+   Font MONTSERRAT = new Font("Montserrat", Font.BOLD, 16);
+   Font MONTSERRAT_MEDIUM = new Font("Montserrat", Font.BOLD, 14);
+
+}
